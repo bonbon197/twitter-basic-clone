0db934b43f3f2bef24cde3a80d4f6bd6bab2a8ed
bonbon <nepunepubon@gmail.com>
2023-12-10 21:46:36 +0800

    Have I not commited this yet

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_10_27_162035) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
